ee3720ed9803395983849d564af5158663e81693
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-04-25 13:18:39 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 232 ms, Memory: 18320 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[] arr = {1,1,2,2,2,8};
+		
+		
+		
+		for(int i =0; i<6 ; i++) {
+			
+			System.out.print(arr[i]-scan.nextInt()+" ");
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
