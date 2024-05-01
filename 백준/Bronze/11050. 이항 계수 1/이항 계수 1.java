387cba4fe65555cc3b808b810ef78fe4c7500fbc
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-05-01 19:59:21 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int n = Integer.parseInt(str.split(" ")[0]);
+        int k = Integer.parseInt(str.split(" ")[1]);
+
+        int result =1;
+
+        for (int i = 0; i <k ; i++) {
+            result*=n--;
+            
+        }
+
+        while(k>1){
+            result/=k--;
+        }
+
+        System.out.println(result);
+
+
+    }
+}
