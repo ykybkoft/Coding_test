7450d56137ea8e46702098c7218e5a85ac59f4b4
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-05-08 22:41:55 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,48 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+        int result =0;
+        int[] arr ={3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10};
+
+        for (char c : word.toCharArray()){
+            result+=arr[(int)c%65];
+        }
+        System.out.println(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
