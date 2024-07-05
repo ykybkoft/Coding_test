ab84d48a2b274f980171ef4e54e4edf9ba3993dc
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-07-05 21:00:00 +0900

    [level 0] Title: 접미사 배열, Time: 0.23 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        answer = new String[my_string.length()];
+
+        for (int i = 0; i < answer.length ; i++) {
+            answer[i] = my_string.substring(answer.length-i-1);
+        }
+
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
