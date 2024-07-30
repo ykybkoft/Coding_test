6c2f9691c9e968ced087f247eb06645d1e750fff
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-07-30 20:15:23 +0900

    [Bronze IV] Title: 녹색거탑, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //bw.flush();
+        //bw.close();
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println((int)Math.pow(2,n));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
