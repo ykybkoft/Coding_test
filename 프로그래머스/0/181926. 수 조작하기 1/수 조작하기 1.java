3931196474226161fe5844c4fe1147f5e9a8c079
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-10-01 21:43:20 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.28 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        for (char c : control.toCharArray()) {
+            switch (c){
+                case 'w':
+                    n+=1;
+                    break;
+                case 's':
+                    n-=1;
+                    break;
+                case 'd':
+                    n+=10;
+                    break;
+                case 'a':
+                    n-=10;
+                    break;
+                        
+                    
+            }
+        }
+        return n;
+    }
+}
